199e2072b757550e3e394504cc024911e0457850
Alexis Cangelosi <cangelosi.alexis@gmail.com>
2025-03-21 13:23:43 +0100

    add version

--- app.py
+++ app.py
@@ -138,6 +138,8 @@ with st.sidebar:
         else:
             st.markdown('<p style="color:red;">⚠️ Unable to retrieve the URLs for the selected clubs.</p>', unsafe_allow_html=True)
 
+    st.caption("v1.0.1")
+    
 st.image(image="assets/logo.png", width=100)
 # --- Menu principal via option_menu ---
 selected_main = option_menu(
